169ca64ca69d96743cd7a80d8ed9dcc4867e47ab
natalia <naholiveiradevargas@gmail.com>
2024-09-01 18:50:49 -0300

    d

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,18 @@
 const express = require("express");
-const path = require("path");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
 const cors = require("cors");
 const { Sequelize, DataTypes } = require("sequelize");
-const AWS = require("aws-sdk");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Configuração do AWS S3
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Coloque sua chave de acesso
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Coloque sua chave secreta
-});
-
-// Configuração do multer para o upload de arquivos para o S3
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.S3_BUCKET_NAME, // Nome do seu bucket S3
-    acl: "public-read", // Permissões de leitura pública
-    key: (req, file, cb) => {
-      cb(null, `users/${Date.now().toString()}_${file.originalname}`); // Define o caminho do arquivo no bucket
-    },
-  }),
-});
-
-// Configuração do Sequelize para conexão com o RDS (PostgreSQL ou MySQL)
+// Configuração do Sequelize para conexão com o banco de dados
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres", // ou 'mysql'
+  dialect: "postgres", // ou 'mysql', dependendo do seu banco de dados
 });
 
 // Definição do modelo User
 const User = sequelize.define("User", {
   name: DataTypes.STRING,
-  photo: DataTypes.STRING,
   points: DataTypes.INTEGER,
 });
 
@@ -50,15 +27,13 @@ app.use(cors());
 // Configuração para analisar o corpo das requisições como JSON
 app.use(express.json());
 
-// Endpoint para adicionar um usuário com foto
-app.post("/api/users", upload.single("photo"), async (req, res) => {
+// Endpoint para adicionar um usuário
+app.post("/api/users", async (req, res) => {
   try {
     const { name } = req.body;
-    const photoUrl = req.file ? req.file.location : ""; // URL da foto no S3
 
     const user = await User.create({
       name,
-      photo: photoUrl,
       points: 0, // Default points
     });
 
