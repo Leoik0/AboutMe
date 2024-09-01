18ab875372d0602a2a5f09057ed1684dc289fa15
natalia <naholiveiradevargas@gmail.com>
2024-09-01 19:29:08 -0300

    f

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const { Sequelize, DataTypes } = require("sequelize");
+import express from "express";
+import cors from "cors";
+import { Sequelize, DataTypes } from "sequelize";
 
 const app = express();
 const port = process.env.PORT || 5000;
