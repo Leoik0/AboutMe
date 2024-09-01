e483dbeb33a899b341603ede55626d53da22354e
natalia <naholiveiradevargas@gmail.com>
2024-09-01 18:45:43 -0300

    g

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
+const multerS3 = require("multer-s3");
 const cors = require("cors");
 const { Sequelize, DataTypes } = require("sequelize");
+const AWS = require("aws-sdk");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Configuração do multer para o upload de arquivos
-const upload = multer({ dest: "uploads/" });
+// Configuração do AWS S3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Coloque sua chave de acesso
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Coloque sua chave secreta
+});
 
-// Configuração do Sequelize para conexão com o SQLite
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.sqlite",
+// Configuração do multer para o upload de arquivos para o S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME, // Nome do seu bucket S3
+    acl: "public-read", // Permissões de leitura pública
+    key: (req, file, cb) => {
+      cb(null, `users/${Date.now().toString()}_${file.originalname}`); // Define o caminho do arquivo no bucket
+    },
+  }),
+});
+
+// Configuração do Sequelize para conexão com o RDS (PostgreSQL ou MySQL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres", // ou 'mysql'
 });
 
 // Definição do modelo User
@@ -34,18 +50,15 @@ app.use(cors());
 // Configuração para analisar o corpo das requisições como JSON
 app.use(express.json());
 
-// Configuração para servir arquivos estáticos
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 // Endpoint para adicionar um usuário com foto
 app.post("/api/users", upload.single("photo"), async (req, res) => {
   try {
     const { name } = req.body;
-    const photoPath = req.file ? req.file.path.replace(/\\/g, "/") : "";
+    const photoUrl = req.file ? req.file.location : ""; // URL da foto no S3
 
     const user = await User.create({
       name,
-      photo: photoPath,
+      photo: photoUrl,
       points: 0, // Default points
     });
 
